61385e3b7f8d69b6efebb8ab32cf06d6f21b84db
Aiden <56921042+1036314134@users.noreply.github.com>
2025-12-06 12:01:24 +0800

    add exchange_rate exam result

--- deeplearning_models/exchange_rate.sh
+++ deeplearning_models/exchange_rate.sh
@@ -14,7 +14,7 @@ for attr in OT; do
     --features M \
     --seq_len 96 \
     --label_len 48 \
-    --pred_len 720 \
+    --pred_len 96 \
     --e_layers 2 \
     --d_layers 1 \
     --factor 3 \
@@ -41,7 +41,7 @@ for attr in OT; do
         --features M \
         --seq_len 96 \
         --label_len 48 \
-        --pred_len 720 \
+        --pred_len 96 \
         --e_layers 2 \
         --d_layers 1 \
         --factor 3 \
@@ -54,6 +54,170 @@ for attr in OT; do
         --des 'Exp' \
         --itr 1 \
         --train_epochs 1
+    done
+  done
+done
+
+
+model_name=Mamba
+
+for attr in OT; do
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id exchange_rate \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --enc_in 8 \
+  --expand 2 \
+  --d_ff 16 \
+  --d_conv 4 \
+  --c_out 8 \
+  --d_model 128 \
+  --des 'Exp' \
+  --itr 1
+
+  for missing_rate in 0.1 0.2 0.3 0.4 0.5; do
+    for method in mean knn xgboost iim front; do
+      python -u run.py \
+        --task_name long_term_forecast \
+        --is_training 1 \
+        --root_path ../dataset/exchange_rate/ \
+        --data_path exchange_rate_missing_${attr}_${missing_rate}_imputed_by_${method}.csv \
+        --model_id exchange_rate_missing_${attr}_${missing_rate}_imputed_by_${method} \
+        --model $model_name \
+        --data custom \
+        --features M \
+        --seq_len 96 \
+        --label_len 48 \
+        --pred_len 96 \
+        --e_layers 2 \
+        --d_layers 1 \
+        --enc_in 8 \
+        --expand 2 \
+        --d_ff 16 \
+        --d_conv 4 \
+        --c_out 8 \
+        --d_model 128 \
+        --des 'Exp' \
+        --itr 1
+
+    done
+  done
+done
+
+
+model_name=Nonstationary_Transformer
+
+for attr in OT; do
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id exchange_rate \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --itr 1 \
+  --p_hidden_dims 256 256 \
+  --p_hidden_layers 2
+
+  for missing_rate in 0.1 0.2 0.3 0.4 0.5; do
+    for method in mean knn xgboost iim front; do
+      python -u run.py \
+        --task_name long_term_forecast \
+        --is_training 1 \
+        --root_path ../dataset/exchange_rate/ \
+        --data_path exchange_rate_missing_${attr}_${missing_rate}_imputed_by_${method}.csv \
+        --model_id exchange_rate_missing_${attr}_${missing_rate}_imputed_by_${method} \
+        --model $model_name \
+        --data custom \
+        --features M \
+        --seq_len 96 \
+        --label_len 48 \
+        --pred_len 96 \
+        --e_layers 2 \
+        --d_layers 1 \
+        --factor 3 \
+        --enc_in 8 \
+        --dec_in 8 \
+        --c_out 8 \
+        --des 'Exp' \
+        --itr 1 \
+        --p_hidden_dims 256 256 \
+        --p_hidden_layers 2
+
+    done
+  done
+done
+
+
+model_name=PatchTST
+
+for attr in OT; do
+  python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id exchange_rate \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --itr 1
+
+  for missing_rate in 0.1 0.2 0.3 0.4 0.5; do
+    for method in mean knn xgboost iim front; do
+      python -u run.py \
+        --task_name long_term_forecast \
+        --is_training 1 \
+        --root_path ../dataset/exchange_rate/ \
+        --data_path exchange_rate_missing_${attr}_${missing_rate}_imputed_by_${method}.csv \
+        --model_id exchange_rate_missing_${attr}_${missing_rate}_imputed_by_${method} \
+        --model $model_name \
+        --data custom \
+        --features M \
+        --seq_len 96 \
+        --label_len 48 \
+        --pred_len 96 \
+        --e_layers 2 \
+        --d_layers 1 \
+        --factor 3 \
+        --enc_in 8 \
+        --dec_in 8 \
+        --c_out 8 \
+        --des 'Exp' \
+        --itr 1
+
     done
   done
 done

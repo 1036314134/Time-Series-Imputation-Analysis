af514bd4ec5be90d60d5f9f1c1c8e5b96ca7f77d
Aiden <56921042+1036314134@users.noreply.github.com>
2026-01-31 15:27:38 +0800

    add etth1 results

--- deeplearning_models/测试脚本.sh
+++ deeplearning_models/测试脚本.sh
@@ -1,95 +1,118 @@
 export CUDA_VISIBLE_DEVICES=0
 
-model_name=TimesNet
-
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ../dataset/illness/ \
-  --data_path national_illness.csv \
-  --model_id ili_36_24 \
-  --model $model_name \
-  --data custom \
-  --features M \
-  --seq_len 36 \
-  --label_len 18 \
-  --pred_len 24 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --d_model 768 \
-  --d_ff 768 \
-  --top_k 5 \
-  --des 'Exp' \
-  --itr 1
-
-model_name=Nonstationary_Transformer
-
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ../dataset/illness/ \
-  --data_path national_illness.csv \
-  --model_id ili_36_24 \
-  --model $model_name \
-  --data custom \
-  --features M \
-  --seq_len 36 \
-  --label_len 18 \
-  --pred_len 24 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1 \
-  --p_hidden_dims 32 32 \
-  --p_hidden_layers 2
-
-model_name=PatchTST
-
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ../dataset/illness/ \
-  --data_path national_illness.csv \
-  --model_id ili_36_24 \
-  --model $model_name \
-  --data custom \
-  --features M \
-  --seq_len 36 \
-  --label_len 18 \
-  --pred_len 24 \
-  --e_layers 4 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --n_heads 4 \
-  --d_model 1024\
-  --itr 1
+#model_name=TimesNet
+#
+#python -u run.py \
+#  --task_name long_term_forecast \
+#  --is_training 1 \
+#  --root_path ../dataset/illness/ \
+#  --data_path national_illness.csv \
+#  --model_id ili_36_24 \
+#  --model $model_name \
+#  --data custom \
+#  --features M \
+#  --seq_len 36 \
+#  --label_len 18 \
+#  --pred_len 24 \
+#  --e_layers 2 \
+#  --d_layers 1 \
+#  --factor 3 \
+#  --enc_in 7 \
+#  --dec_in 7 \
+#  --c_out 7 \
+#  --d_model 768 \
+#  --d_ff 768 \
+#  --top_k 5 \
+#  --des 'Exp' \
+#  --itr 1
+#
+#model_name=Nonstationary_Transformer
+#
+#python -u run.py \
+#  --task_name long_term_forecast \
+#  --is_training 1 \
+#  --root_path ../dataset/illness/ \
+#  --data_path national_illness.csv \
+#  --model_id ili_36_24 \
+#  --model $model_name \
+#  --data custom \
+#  --features M \
+#  --seq_len 36 \
+#  --label_len 18 \
+#  --pred_len 24 \
+#  --e_layers 2 \
+#  --d_layers 1 \
+#  --factor 3 \
+#  --enc_in 7 \
+#  --dec_in 7 \
+#  --c_out 7 \
+#  --des 'Exp' \
+#  --itr 1 \
+#  --p_hidden_dims 32 32 \
+#  --p_hidden_layers 2
+#
+#model_name=PatchTST
+#
+#python -u run.py \
+#  --task_name long_term_forecast \
+#  --is_training 1 \
+#  --root_path ../dataset/illness/ \
+#  --data_path national_illness.csv \
+#  --model_id ili_36_24 \
+#  --model $model_name \
+#  --data custom \
+#  --features M \
+#  --seq_len 36 \
+#  --label_len 18 \
+#  --pred_len 24 \
+#  --e_layers 4 \
+#  --d_layers 1 \
+#  --factor 3 \
+#  --enc_in 7 \
+#  --dec_in 7 \
+#  --c_out 7 \
+#  --des 'Exp' \
+#  --n_heads 4 \
+#  --d_model 1024\
+#  --itr 1
+#
+#model_name=Autoformer
+#
+#python -u run.py \
+#  --task_name long_term_forecast \
+#  --is_training 1 \
+#  --root_path ../dataset/illness/ \
+#  --data_path national_illness.csv \
+#  --model_id ili_36_24 \
+#  --model $model_name \
+#  --data custom \
+#  --features M \
+#  --seq_len 36 \
+#  --label_len 18 \
+#  --pred_len 24 \
+#  --e_layers 2 \
+#  --d_layers 1 \
+#  --factor 3 \
+#  --enc_in 7 \
+#  --dec_in 7 \
+#  --c_out 7 \
+#  --des 'Exp' \
+#  --itr 1
 
 model_name=Autoformer
 
 python -u run.py \
   --task_name long_term_forecast \
   --is_training 1 \
-  --root_path ../dataset/illness/ \
-  --data_path national_illness.csv \
-  --model_id ili_36_24 \
+  --root_path ../dataset/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_96 \
   --model $model_name \
-  --data custom \
+  --data ETTh1 \
   --features M \
-  --seq_len 36 \
-  --label_len 18 \
-  --pred_len 24 \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
   --e_layers 2 \
   --d_layers 1 \
   --factor 3 \

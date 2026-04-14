d736fa8be4a911f2f1b3c009daf30683d7cdc560
Aiden <56921042+1036314134@users.noreply.github.com>
2026-04-14 14:35:17 +0800

    删除delete imputer

--- imputer/delete.py
+++
@@ -1,4 +0,0 @@
-def delete_missing(raw_feature_df):
-    imputed_feature_df = raw_feature_df.copy()
-    imputed_feature_df = imputed_feature_df.dropna(axis=0, how='any')
-    return imputed_feature_df
